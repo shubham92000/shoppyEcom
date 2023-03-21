74b512f7a2196436fa2e289f7fa8593bb0f85d9b
shubham92000 <sbhsingh616@gmail.com>
2023-03-21 17:39:13 +0530

    init and added bootstrap and jquery with index files

---
+++ shoppyWebParent/shoppyBackend/src/main/java/com/shoppy/admin/ShoppyBackendApplication.java
@@ -0,0 +1,11 @@
+package com.shoppy.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppyBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppyBackendApplication.class, args);
+    }
+}
---
+++ shoppyWebParent/shoppyFrontend/src/main/java/com/shoppy/site/ShoppyFrontEndApplication.java
@@ -0,0 +1,11 @@
+package com.shoppy.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppyFrontEndApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppyFrontEndApplication.class, args);
+    }
+}
