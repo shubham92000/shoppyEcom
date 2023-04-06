71283dce1b63322faa18fefb1af3e04f018aa3d8
shubham92000 <sbhsingh616@gmail.com>
2023-04-06 12:44:30 +0530

    added password encoder

---
+++ shoppyWebParent/shoppyBackend/src/test/java/com/shoppy/admin/user/PasswordEncoderTest.java
@@ -0,0 +1,18 @@
+package com.shoppy.admin.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PasswordEncoderTest {
+    @Test
+    public void testEncodePassword () {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String rawPassword = "password";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        System.out.println(encodedPassword);
+        Boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
+        assertThat(matches).isTrue();
+    }
+}
