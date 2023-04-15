010d3bfb0f19184b8487c1f12791390e230f4c6c
shubham92000 <sbhsingh616@gmail.com>
2023-04-15 16:24:45 +0530

    add and show user photos

---
+++ shoppyWebParent/shoppyBackend/src/main/java/com/shoppy/admin/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.shoppy.admin;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String dirName = "user-photos";
+        Path userPhotosDir = Paths.get(dirName);
+        String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
+        System.out.println("/" + dirName + "/**");
+        System.out.println("file:/" + userPhotosPath + "/");
+        registry.addResourceHandler("/" + dirName + "/**")
+                .addResourceLocations("file:/" + userPhotosPath + "/");
+    }
+}
--- shoppyWebParent/shoppyBackend/src/main/java/com/shoppy/admin/user/UserService.java
+++ shoppyWebParent/shoppyBackend/src/main/java/com/shoppy/admin/user/UserService.java
@@ -31,7 +31,7 @@ public class UserService {
         return (List<Role>) roleRepository.findAll();
     }
 
-    public void save(User user){
+    public User save(User user){
         boolean isUpdatingUser = user.getId()!=null;
         if(isUpdatingUser) {
             User existingUser = userRepository.findById(user.getId()).get();
@@ -45,7 +45,7 @@ public class UserService {
         else {
             encodePassword(user);
         }
-        userRepository.save(user);
+        return userRepository.save(user);
     }
 
     private void encodePassword(User user) {
